20ac1f929495b027a052284d164437649cdf234d
radishrp <radishrp@gmail.com>
2014-01-12 23:52:28 -0800

    Fixes and updates
    Fixed win message
    Fixed char type - declare all functions at the start
    Updated "graphics render"

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 //int init_board(tictactoeboard *board) {
  //       int i = 0;
@@ -14,37 +14,46 @@
 typedef struct tictactoeboard_description {
         char square[3][3];
         }tictactoeboard; //structure of tictactoeboard maybe used in later edits
+int checkwin(char *, char);
+void winmessage(int, char);
 
 
 
 
 
 
-int main()
-{
+int main(){
 
-char inputdata[]="xxx oo xo";
-    display_tictactoe_boardarray(inputdata);
-return 1;
+char inputdata[]="XXX OO XO";
+display_tictactoe_boardarray(inputdata);
+char player = 'X';
+int win_position_return_value = checkwin(inputdata,player);
+winmessage(win_position_return_value,player);
+
+
+
+
+};
+
+void winmessage(int win_position_return_value, char player){
+switch(win_position_return_value){
+    case 10 : printf("Horizontal win on Row 1 by %c!",player); break;
+    case 13 : printf("Horizontal win on Row 2 by %c!",player); break;
+    case 16 : printf("Horizontal win on Row 3 by %c!",player); break;
+    case 20 : printf("Vertical win on Column 1 by %c!",player); break;
+    case 23 : printf("Vertical win on Column 2 by %c!",player); break;
+    case 26 : printf("Vertical win on Column 3 by %c!",player); break;
+    case 30 : printf("Diagonal win by %c!", player); break;
+    default: printf("Error."); break;
+
+};
 };
 
 
 
-void checkwin(char *inputdata, char player) {
-    int i;
-    for(i=0;i<7;i+=3){
-        if(inputdata[i]== player && inputdata[i+1]==player && inputdata[i+2]==player){
-            return 1;};
-                         }; //horizontal win condition
-    for(i=0;i<3;i++){
-        if(inputdata[i]==player && inputdata[i+3]==player && inputdata[i+6]==player){
-            return 1;}
-                        }; //vertical win condition
-    if((inputdata[0]==player && inputdata[4]==player && inputdata[8]==player) || (inputdata[2]==player && inputdata[4]==player && inputdata[6]==player)){
-        return 1; // diagonal win condition
-                                                                                                                                    };
-    return 0;
-                                                                                                                                    };
+
+
+
 
 
 void display_tictactoe_boardarray(char *inputdata) {
@@ -66,8 +75,25 @@ void display_tictactoe_boardarray(char *inputdata) {
 
         };
     };
+
+
+};
+
+int checkwin(char *inputdata,char player){
+    int i;
+    for(i=0;i<7;i+=3){
+        if(inputdata[i]== player && inputdata[i+1]== player && inputdata[i+2]== player){
+            return (10+i);};
+                         }; //horizontal win condition
+    for(i=0;i<3;i++){
+        if(inputdata[i]== player && inputdata[i+3]== player && inputdata[i+6]== player){
+            return (20+i);}
+                        }; //vertical win condition
+    if((inputdata[0]== player && inputdata[4]== player && inputdata[8]== player) || (inputdata[2]== player && inputdata[4]== player && inputdata[6]== player)){
+        return 30; // diagonal win condition
+                                                                                                                                    };
+
+return 0;
 }
-
-
 
      // for loop allows input into the board
